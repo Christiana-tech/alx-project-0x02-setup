1363c1072e8d914910b3d14761da8e12f2e5e6b3
Christiana-tech <ojofayo56@gmail.com>
2025-01-12 16:05:54 +0100

    commit all

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '@/components/layout/Header';
 import PostCard from '@/components/common/PostCard';
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
